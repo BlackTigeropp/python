4a976a37edafc4210a5883bd543466f7cc57bb49
BlackTigeropp <hussinekibar41@gmail.com>
2021-05-07 12:59:54 +0300

    condtions

---
+++ condtions.py
@@ -0,0 +1,6 @@
+uesrname = input('Please insert your username: ')
+password = input('please insert your password: ')
+if username == 'hussain' and password == '123':
+    print('wellcome to your email')
+else:
+    print('Error')
---
+++ operators.py
@@ -0,0 +1,31 @@
+# num1 = 5 
+# num2 = 6
+# print(num1 > num2 )
+# print(num1 < num2 )
+# print(5 == 6)
+# print(5 == 5)
+# print(5 != 6)
+# print(5 >=6)
+# print(7 >= 6)
+# print(6 >= 7) 
+# print(5 <= 6)
+
+# print(True and True) #True 
+# print(True and False) #False
+# print(False and False) #False
+a,b = 1,2
+# print(a ==b and b ==b ) #true
+# print(a == a and b < a) #false 
+# print(a > b and b < a)
+
+# print(True or True) #true
+# print(True or False) #true
+# print(False or False) #false 
+# print(a == a or b ==b) #true 
+# print(a == a or b < a) #true
+# print(a > b or b < a) #false
+
+# print(not True) #false
+# print(not False) #true 
+# print(not (a !=5)) #false
+# print(not (a == 5)) #true 
